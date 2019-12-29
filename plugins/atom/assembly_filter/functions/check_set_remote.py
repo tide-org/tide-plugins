d35dc55ddda7b644df17c7cd10e5367834aeb1df
Willem van Ketwich <willvk@gmail.com>
2019-12-29 18:28:35 +1100

    refactor old code, remove filters path

--- plugins/atom/assembly_filter/functions/check_set_remote.py
+++ plugins/atom/assembly_filter/functions/check_set_remote.py
@@ -1,18 +1,14 @@
-from config import Config
+from tide.config.config import Config
 
 def check_set_remote(command_args):
     if 'target remote' in command_args["process_command"].lower():
         binary_loaded = False
-        no_symbols_found = False
+        symbols_found = True
         for line in command_args["lines"]:
-            if 'Reading symbols from' in line:
+            if 'Reading symbols from' in line or '(no debugging symbols found)' in line:
                 binary_loaded = True
             if '(no debugging symbols found)' in line:
-                no_symbols_found = True
-                binary_loaded = True
-        symbols_loaded = not no_symbols_found
-        if symbols_loaded and not binary_loaded:
-            symbols_loaded = False
-        Config().get()['variables']['remote_target'] = 1
-        Config().get()['variables']['binary_loaded'] = int(binary_loaded)
-        Config().get()['variables']['symbols_loaded'] = int(symbols_loaded)
+                symbols_found = False
+        Config().set_variable('remote_target', 1)
+        Config().set_variable('binary_loaded', int(binary_loaded))
+        Config().set_variable('symbols_loaded', int(symbols_found and binary_loaded))
